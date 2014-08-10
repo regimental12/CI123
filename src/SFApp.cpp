fe9545592dd2b8ae471d145c19306d409d4cec9c
Richard hankins <rhankins@localhost.localdomain>
2014-08-10 14:07:00 +0100

    redone first commit

---
+++ src/SFApp.cpp
@@ -0,0 +1,135 @@
+#include "SFApp.h"
+
+SFApp::SFApp() : fire(0), is_running(true) {
+
+  surface = SDL_GetVideoSurface();
+  app_box = make_shared<SFBoundingBox>(Vector2(surface->w/2, surface->h/2), surface->w/2, surface->h/2);
+  player  = make_shared<SFAsset>(SFASSET_PLAYER);
+  auto player_pos = Point2(surface->w/2, 88.0f);
+  player->SetPosition(player_pos);
+
+  const int number_of_aliens = 10;
+  for(int i=0; i<number_of_aliens; i++) {
+    // place an alien at width/number_of_aliens * i
+    auto alien = make_shared<SFAsset>(SFASSET_ALIEN);
+    auto pos   = Point2((surface->w/number_of_aliens) * i, 200.0f);
+    alien->SetPosition(pos);
+    aliens.push_back(alien);
+  }
+
+  auto coin = make_shared<SFAsset>(SFASSET_COIN);
+  auto pos  = Point2((surface->w/4), 100);
+  coin->SetPosition(pos);
+  coins.push_back(coin);
+}
+
+SFApp::~SFApp() {
+}
+
+/**
+ * Handle all events that come from SDL.
+ * These are timer or keyboard events.
+ */
+void SFApp::OnEvent(SFEvent& event) {
+  SFEVENT the_event = event.GetCode();
+  
+  switch (the_event) {
+  case SFEVENT_QUIT:
+    is_running = false;
+    break;
+  case SFEVENT_UPDATE:
+    OnUpdateWorld();
+    OnRender();
+    break;
+  case SFEVENT_PLAYER_LEFT:
+    player->GoWest();
+    break;
+  case SFEVENT_PLAYER_RIGHT:
+    player->GoEast();
+    break;
+  case SFEVENT_FIRE:
+    fire ++;
+    std::stringstream sstm;
+    sstm << "Fire " << fire;
+    SDL_WM_SetCaption(sstm.str().c_str(),  sstm.str().c_str());
+    FireProjectile();
+    break;
+  }
+}
+
+int SFApp::OnExecute() {
+  // Execute the app
+  SDL_Event event;
+  while (SDL_WaitEvent(&event) && is_running) {
+    // if this is an update event, then handle it in SFApp,
+    // otherwise punt it to the SFEventDispacher.
+    SFEvent sfevent((const SDL_Event) event);
+    OnEvent(sfevent);
+  }
+}
+
+void SFApp::OnUpdateWorld() {
+  // Update projectile positions
+  for(auto p: projectiles) {
+    p->GoNorth();
+  }
+
+  for(auto c: coins) {
+    c->GoSouth();
+  }
+
+  // Update enemy positions
+  for(auto a : aliens) {
+    // do something here
+  }
+
+  // Detect collisions
+  for(auto p : projectiles) {
+    for(auto a : aliens) {
+      if(p->CollidesWith(a)) {
+        p->HandleCollision();
+        a->HandleCollision();
+      }
+    }
+  }
+
+  // remove dead aliens (the long way)
+  list<shared_ptr<SFAsset>> tmp;
+  for(auto a : aliens) {
+    if(a->IsAlive()) {
+      tmp.push_back(a);
+    }
+  }
+  aliens.clear();
+  aliens = list<shared_ptr<SFAsset>>(tmp);
+}
+
+void SFApp::OnRender() {
+  // clear the surface
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 8, 54, 129) );
+
+  // draw the player
+  player->OnRender(surface);
+
+  for(auto p: projectiles) {
+    if(p->IsAlive()) {p->OnRender(surface);}
+  }
+
+  for(auto a: aliens) {
+    if(a->IsAlive()) {a->OnRender(surface);}
+  }
+
+  for(auto c: coins) {
+    c->OnRender(surface);
+  }
+
+  // Switch the off-screen buffer to be on-screen
+  SDL_Flip(surface);
+}
+
+void SFApp::FireProjectile() {
+  auto pb = make_shared<SFAsset>(SFASSET_PROJECTILE);
+  auto v  = player->GetPosition();
+  pb->SetPosition(v);
+  projectiles.push_back(pb);
+}
---
+++ src/SFApp.h
@@ -0,0 +1,44 @@
+#ifndef SFAPP_H
+#define SFAPP_H
+
+#include <memory>   // Pull in std::shared_ptr
+#include <iostream> // Pull in std::cerr, std::endl
+#include <list>     // Pull in list
+#include <sstream>
+
+using namespace std;
+
+#include "SFCommon.h"
+#include "SFEvent.h"
+#include "SFAsset.h"
+
+/**
+ * Represents the StarshipFontana application.  It has responsibilities for
+ * * Creating and destroying the app window
+ * * Processing game events
+ */
+class SFApp {
+public:
+  SFApp();
+  virtual ~SFApp();
+  void    OnEvent(SFEvent &);
+  int     OnExecute();
+  void    OnUpdateWorld();
+  void    OnRender();
+
+  void    FireProjectile();
+private:
+  SDL_Surface           * surface;
+  bool                    is_running;
+
+  shared_ptr<SFAsset>        player;
+  shared_ptr<SFBoundingBox>  app_box;
+  list<shared_ptr<SFAsset> > projectiles;
+  list<shared_ptr<SFAsset> > aliens;
+  list<shared_ptr<SFAsset> > coins;
+
+  int fire;
+
+  SFError OnInit();
+};
+#endif
